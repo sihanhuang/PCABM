217509cb0b618946fa5944bddc42600214e68905
Sihan Huang <sihanhuang@Sihans-MBP.cable.rcn.com>
2023-05-27 15:51:23 -0400

    add ta2

---
+++ simulations/ta2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#SBATCH --array=1-100
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --output=../output/ta2/slurm-%a_ta2.log
+
+python ta2.py -n 500 -g 2 -r 4 -seed $SLURM_ARRAY_TASK_ID 
+#python readK.py -dir ta2
