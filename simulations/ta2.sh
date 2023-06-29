4c39fbdd4415a4bd3ed669e05b801bab6576aeae
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:09:54 -0400

    Update ta2.sh

--- simulations/ta2.sh
+++ simulations/ta2.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/ta2/slurm-%a_ta2.log
+#SBATCH --output=/dev/null
 
-python ta2.py -n 500 -g 2 -r 4 -seed $SLURM_ARRAY_TASK_ID r
+logpath="../output/ta2"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_ta2.log"
+
+python ta2.py -n 500 -g 2 -r 4 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readsv.py -dir ta2
