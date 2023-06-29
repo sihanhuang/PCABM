055d327907d44a2c5363bd6afd502882a9f7b00c
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:07:41 -0400

    Update 4b.sh

--- simulations/4b.sh
+++ simulations/4b.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/4b/slurm-%a_4b.log
+#SBATCH --output=/dev/null
 
-python 4b.py -n 200 -seed $SLURM_ARRAY_TASK_ID 
+logpath="../output/4b"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_4b.log"
+
+python 4b.py -n 200 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readRes_score.py -dir 4b
