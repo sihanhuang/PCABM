76ae1906c9be423cf77e33803d6b967ff122d190
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:05:46 -0400

    Update 3b.sh

--- simulations/3b.sh
+++ simulations/3b.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/3b/slurm-%a_3b.log
+#SBATCH --output=/dev/null
 
-python 3b.py -n 200 -g 1.2 -seed $SLURM_ARRAY_TASK_ID 
+logpath="../output/3b"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_3b.log"
+
+python 3b.py -n 200 -g 1.2 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readRes.py -dir 3b
