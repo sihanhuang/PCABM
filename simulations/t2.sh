a5ce7723ab1156ef5de93164d15b206fdf120e2d
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:08:55 -0400

    Update t2.sh

--- simulations/t2.sh
+++ simulations/t2.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/t2/slurm-%a_t2.log
+#SBATCH --output=/dev/null
 
-python t2.py -n 1000 -g 1 -r 5 -seed $SLURM_ARRAY_TASK_ID 
+logpath="../output/t2"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_t2.log"
+
+python t2.py -n 1000 -g 1 -r 5 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readK.py -dir t2
