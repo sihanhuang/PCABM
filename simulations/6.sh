ad126cc6230d17fdfddcc8a9761070f78712a03c
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:08:03 -0400

    Update 6.sh

--- simulations/6.sh
+++ simulations/6.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/6/slurm-%a_6.log
+#SBATCH --output=/dev/null
 
-python 6.py -n 500 -g 2 -r 4 -seed $SLURM_ARRAY_TASK_ID 
+logpath="../output/6"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_6.log"
+
+python 6.py -n 500 -g 2 -r 4 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readCov.py -dir 6
