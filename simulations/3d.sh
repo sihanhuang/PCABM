7d8786b51c7ca7cf7895c0da050b4c52e2f413b3
Sihan Huang <sh3453@columbia.edu>
2023-06-29 11:06:50 -0400

    Update 3d.sh

--- simulations/3d.sh
+++ simulations/3d.sh
@@ -3,7 +3,11 @@
 #SBATCH --array=1-100
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=10
-#SBATCH --output=../output/3d/slurm-%a_3d.log
+#SBATCH --output=/dev/null
 
-python 3d.py -n 200 -g 1.5 -r 2 -seed $SLURM_ARRAY_TASK_ID 
+logpath="../output/3d"
+mkdir -p $logpath
+logfile="$logpath/slurm-${SLURM_ARRAY_TASK_ID}_3d.log"
+
+python 3d.py -n 200 -g 1.5 -r 2 -seed $SLURM_ARRAY_TASK_ID > ${logfile}
 #python readInit.py -dir 3d
